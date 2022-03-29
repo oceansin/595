2a6d91f8a0ac73a25ac2d3c17a9b9cbc45a6048e
Clark Kent <Clark Kent@OWin>
2022-03-28 23:46:15 -0700

    new file ocean.java

---
+++ MavenWorld/mavenproject/src/main/java/com/simplilearn/mavenproject/ocean.java
@@ -0,0 +1,10 @@
+package com.simplilearn.mavenproject;
+
+public class ocean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Hello Ocean");
+	}
+
+}
